c6d4b902781bec02602377b607f3dc72fb36c6d0
Charlie Heger <charlieheger500@gmail.com>
2026-06-30 12:13:37 -0500

    Add: car-fleet - submission-23

---
+++ Data Structures & Algorithms/car-fleet/submission-23.py
@@ -0,0 +1,48 @@
+def willMeet(car1: int, car2: int, target: int, position: List[int], speed: List[int]):
+    leadCar = None
+    trailCar = None
+
+    if position[car1] > position[car2]:
+        leadCar = car1
+        trailCar = car2
+    else:
+        leadCar = car2
+        trailCar = car1
+
+    leadCarTime = (target - position[leadCar]) / speed[leadCar]
+    trailCarTime = (target - position[trailCar]) / speed[trailCar]
+
+    print("Leadcar pos:", position[leadCar])
+    print("Leadcar spd:", speed[leadCar])
+    print("Leadcar time:", leadCarTime)
+    print()
+    print("Trailcar pos:", position[trailCar])
+    print("Trailcar spd:", speed[trailCar])
+    print("Trailcar time:", trailCarTime)
+    print('\n')
+
+    if leadCarTime >= trailCarTime: 
+        return True
+    else:
+        return False
+
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        timeToTarget = []
+
+        order = [(p, i) for i, p in enumerate(position)]
+        order.sort(reverse=True)
+        print(order)
+
+        stack = []
+
+        for p, i in order:
+            time = (target - position[i]) / speed[i]
+
+            if not stack or time > stack[-1]:
+                stack.append(time)
+
+        print(stack)
+        return len(stack)
+
